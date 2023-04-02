15759bc071da988587125f41e7f26205f0780fc3
Zaharia Diana Cristiana <dianacristiana2002@gmail.com>
2023-04-02 19:42:13 +0300

    Add files via upload

---
+++ PAO-projectt/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+package src;
+
+import src.produse.Produse;
+import src.produse.Carte;
+import src.produse.Manual;
+import src.produse.Culegere;
+
+
+import static src.servicii.Service.menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        menu();
+    }
+}
